5e573c056b13aa363376b851c7850f1832bee295
Nube28 <159978169+Nube28@users.noreply.github.com>
2025-02-04 22:32:22 -0700

    Navegacion realizada

--- PapuCheezery/app/src/main/java/gomez/alejandro/papucheezery/MainActivity.kt
+++ PapuCheezery/app/src/main/java/gomez/alejandro/papucheezery/MainActivity.kt
@@ -1,6 +1,8 @@
 package gomez.alejandro.papucheezery
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val button:Button = findViewById(R.id.button_get_start) as Button
+        button.setOnClickListener{
+            var intent: Intent = Intent(this, RegistrerActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- PapuCheezery/app/src/main/java/gomez/alejandro/papucheezery/RegistrerActivity.kt
+++ PapuCheezery/app/src/main/java/gomez/alejandro/papucheezery/RegistrerActivity.kt
@@ -1,6 +1,8 @@
 package gomez.alejandro.papucheezery
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class RegistrerActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val button: Button = findViewById(R.id.sign_in_c) as Button
+        button.setOnClickListener{
+            var intent: Intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
         }
     }
 }
