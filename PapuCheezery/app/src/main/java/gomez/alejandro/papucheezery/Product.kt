6de7fa605d6f33a1614f20778e4445eaf36919b8
oloo <ooo>
2025-02-11 17:50:46 -0700

    avance pro

---
+++ PapuCheezery/app/src/main/java/gomez/alejandro/papucheezery/Product.kt
@@ -0,0 +1,8 @@
+package gomez.alejandro.papucheezery
+
+data class Product (var name: String,
+                    var image: Int,
+                    var descrption: String,
+                    var price: Double){
+
+}
